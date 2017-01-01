199582f43c60009451ce650cc915421f19414879
jeobmallari <jeobmallari@gmail.com>
2017-01-02 02:04:20 +0800

    Added creating html file

--- WebServer.java
+++ WebServer.java
@@ -1,4 +1,6 @@
+import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -8,7 +10,7 @@ public class WebServer {
 
   protected void start() {
     ServerSocket s;
-
+    File f;
     System.out.println("Webserver starting up on port 80");
     System.out.println("(press ctrl-c to exit)");
     try{
@@ -19,14 +21,16 @@ public class WebServer {
     }
 
     System.out.println("Waiting for connection");
+    f = new File("WebServer.html");
     for (;;) {
 		try {
 			Socket remote = s.accept();
 			System.out.println("Connection, sending data.");
 			BufferedReader in = new BufferedReader(new InputStreamReader(remote.getInputStream()));
 			PrintWriter out = new PrintWriter(remote.getOutputStream());
-
+			PrintWriter toFile = new PrintWriter(new BufferedWriter(new FileWriter(f)));
 			String[] htmlReq = new String[64];
+
 			String str = ".";
 			int a = 0;
 			while (!str.equals("")){
@@ -42,7 +46,16 @@ public class WebServer {
 		out.println("<H1>CMSC 137 Lecture Project 2 (Web Server)</H1>\n");
 		out.println("<p>HTML REQUEST: </br>");
 		out.println("<table border=\"1\" style=\"width:100%\">");
-		
+/*
+		toFile.println("HTTP/1.0 200 OK");
+		toFile.println("Content-Type: text/html");
+		toFile.println("Server: Bot");
+		toFile.println("DirectoryIndex 8080 project.html");
+		toFile.println("");
+		toFile.println("<H1>CMSC 137 Lecture Project 2 (Web Server)</H1>\n");
+		toFile.println("<p>HTML REQUEST: </br>");
+		toFile.println("<table border=\"1\" style=\"width:100%\">");
+*/		
 		for(int i=0;i<a-1;i++){
 			String[] tokens = htmlReq[i].split(" ");
 			int length = tokens.length;
@@ -51,6 +64,12 @@ public class WebServer {
 			out.println("<td>");
 			out.println(tokens[0]);
 			out.println("</td>");
+
+			toFile.println("<tr>");
+			toFile.println("<td>");
+			toFile.println(tokens[0]);
+			toFile.println("</td>");
+
         	
         	for(int b=1;b<length;b++){
         		rest += tokens[b] + " ";
@@ -60,9 +79,17 @@ public class WebServer {
 			out.println(rest);
 			out.println("</td>");
 			out.println("</tr>");
+
+			toFile.println("<td>");
+			toFile.println(rest);
+			toFile.println("</td>");
+			toFile.println("</tr>");
 		}
 		out.println("</table>");
 		out.flush();
+
+		toFile.println("</table>");
+		toFile.flush();
 			remote.close();
 		}catch(Exception e) {
 			System.out.println("Error: " + e);
