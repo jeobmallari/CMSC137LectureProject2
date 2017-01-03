baaf1607014e207b5a4343d5eaf34e1e405df2c2
jeobmallari <jeobmallari@gmail.com>
2017-01-04 02:09:40 +0800

    Update WebServer.java

--- WebServer.java
+++ WebServer.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -21,75 +22,82 @@ public class WebServer {
     }
 
     System.out.println("Waiting for connection");
-    f = new File("WebServer.html");
     for (;;) {
 		try {
 			Socket remote = s.accept();
 			System.out.println("Connection, sending data.");
 			BufferedReader in = new BufferedReader(new InputStreamReader(remote.getInputStream()));
 			PrintWriter out = new PrintWriter(remote.getOutputStream());
-			PrintWriter toFile = new PrintWriter(new BufferedWriter(new FileWriter(f)));
 			String[] htmlReq = new String[64];
 
 			String str = ".";
 			int a = 0;
+			String str1 = in.readLine();
 			while (!str.equals("")){
-			str = in.readLine();
-			htmlReq[a] = str+"</br>";
-			a++;
-        }
-		out.println("HTTP/1.0 200 OK");
-		out.println("Content-Type: text/html");
-		out.println("Server: Bot");
-		out.println("DirectoryIndex 8080 project.html");
-		out.println("");
-		out.println("<H1>CMSC 137 Lecture Project 2 (Web Server)</H1>\n");
-		out.println("<p>HTML REQUEST: </br>");
-		out.println("<table border=\"1\" style=\"width:100%\">");
-/*
-		toFile.println("HTTP/1.0 200 OK");
-		toFile.println("Content-Type: text/html");
-		toFile.println("Server: Bot");
-		toFile.println("DirectoryIndex 8080 project.html");
-		toFile.println("");
-		toFile.println("<H1>CMSC 137 Lecture Project 2 (Web Server)</H1>\n");
-		toFile.println("<p>HTML REQUEST: </br>");
-		toFile.println("<table border=\"1\" style=\"width:100%\">");
-*/		
-		for(int i=0;i<a-1;i++){
-			String[] tokens = htmlReq[i].split(" ");
-			int length = tokens.length;
-			String rest = "";
-			out.println("<tr>");
-			out.println("<td>");
-			out.println(tokens[0]);
-			out.println("</td>");
+				str = in.readLine();
+				htmlReq[a] = str+"</br>";
+				a++;
+	        }
+			out.println("HTTP/1.0 200 OK");
+			out.println("Content-Type: text/html");
+			out.println("Server: Bot");
+			out.println("DirectoryIndex 8080 project.html");
+			out.println("");
+			out.println("<H1>CMSC 137 Lecture Project 2 (Web Server)</H1>\n");
+			out.println("<p>HTML REQUEST: </br>");
+			out.println("<table border=\"1\" style=\"width:100%\">");
+			
+			String[] headers = str1.split(" ");
+			out.println("<tr>"
+				+"<td>"
+				+"Method:"
+				+"</td>"
+				+"<td>"
+				+headers[0]
+				+"</td>"
+				+"</tr>");
 
-			toFile.println("<tr>");
-			toFile.println("<td>");
-			toFile.println(tokens[0]);
-			toFile.println("</td>");
+			out.println("<tr>"
+				+"<td>"
+				+"URI:"
+				+"</td>"
+				+"<td>"
+				+headers[1]
+				+"</td>"
+				+"</tr>");
 
-        	
-        	for(int b=1;b<length;b++){
-        		rest += tokens[b] + " ";
-        	}
-        	
-			out.println("<td>");
-			out.println(rest);
-			out.println("</td>");
-			out.println("</tr>");
+			for(int i=0;i<a-1;i++){
+				String[] tokens = htmlReq[i].split(" ");
+				int length = tokens.length;
+				String rest = "";
+				out.println("<tr>");
+				out.println("<td>");
+				out.println(tokens[0]);
+				out.println("</td>");
+	        	
+	        	for(int b=1;b<length;b++){
+	        		rest += tokens[b] + " ";
+	        	}
+	        	
+				out.println("<td>");
+				out.println(rest);
+				out.println("</td>");
+				out.println("</tr>");
 
-			toFile.println("<td>");
-			toFile.println(rest);
-			toFile.println("</td>");
-			toFile.println("</tr>");
-		}
-		out.println("</table>");
-		out.flush();
+			}
+			out.println("</table>");
 
-		toFile.println("</table>");
-		toFile.flush();
+			String filename = headers[1].substring(1);
+
+			f = new File(filename);
+			if(!f.exists()){
+				out.println("<h1>File Does Not Exist.</h1>");
+			}
+			else{
+				
+			}
+
+			out.flush();
 			remote.close();
 		}catch(Exception e) {
 			System.out.println("Error: " + e);
